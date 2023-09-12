09f48310e3de7b10c3d804840f6f99b3c0560b97
perso <elisa.github@gmail.com>
2023-09-12 20:52:11 +0200

    add com error

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,9 @@
-import { NavLink } from "react-router-dom";
+// Ce code crée un composant React "Error" qui représente une page d'erreur 404.
+// Il affiche un message d'erreur personnalisé et un lien pour retourner à la page d'accueil en utilisant NavLink.
 
+import { NavLink } from "react-router-dom";  // Import de NavLink depuis React Router
+
+// Définition du composant "Error"
 function Error() {
   return (
     <div className="errorContent">
