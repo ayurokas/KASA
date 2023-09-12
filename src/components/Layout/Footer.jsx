4ca20f283fc8befd1f20ee0a94164a53eabc52e0
perso <elisa.github@gmail.com>
2023-09-12 20:42:36 +0200

    add com footer

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,9 +1,14 @@
+// Ce code crée un composant React "Footer" qui représente la partie inférieure de la page,
+// généralement utilisée pour afficher des informations de pied de page, comme le logo et le copyright.
+
 import { NavLink } from "react-router-dom";
 import Logo from "../../assets/kasa_logo_white.svg";
 
+// Définition du composant "Footer"
 function Footer() {
   return (
     <footer>
+       {/* Lien vers la page d'accueil avec le logo */}
       <NavLink to="/">
         <img src={Logo} className="footer__logo" alt="Kasa Logo" />
       </NavLink>
